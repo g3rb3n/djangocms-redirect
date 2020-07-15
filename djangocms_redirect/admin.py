39c49a9b238f0723197fc347121c2fae6137d42e
Gerben <gerbenvaneerten@gmail.com>
2020-07-15 17:03:01 +0200

    Made site field hidden

--- djangocms_redirect/admin.py
+++ djangocms_redirect/admin.py
@@ -3,7 +3,8 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from cms.forms.widgets import PageSmartLinkWidget
 from django.contrib import admin
-from django.forms import ModelForm
+from django.contrib.sites.models import Site
+from django.forms import ModelForm, HiddenInput
 from django.utils.translation import get_language
 
 from .models import Redirect
@@ -18,6 +19,8 @@ class RedirectForm(ModelForm):
         super(RedirectForm, self).__init__(*args, **kwargs)
         widget = PageSmartLinkWidget(ajax_view='admin:cms_page_get_published_pagelist')
         widget.language = get_language()
+        self.fields['site'].widget = HiddenInput()
+        self.fields['site'].initial = Site.objects.get_current().pk
         self.fields['old_path'].widget = widget
         self.fields['new_path'].widget = widget
 
